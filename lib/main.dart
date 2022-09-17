2a184c6f3fddf0ef681e9890af7c2611dcec9bf5
Alireza Naseri <alireza.real98@gmail.com>
2022-09-17 14:10:58 +0430

    inactive debug show banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Navigate to new screen',
       home: Home(),
     );
