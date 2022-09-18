0c4fc6de3ea3db168d8df11f57e6b3906558a5c4
Alireza Naseri <alireza.real98@gmail.com>
2022-09-18 10:06:05 +0430

    write body for login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  LoginPage({required String email, required String password});
+  String email;
+  String password;
+  LoginPage({Key? key, required this.email, required this.password}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +13,13 @@ class LoginPage extends StatelessWidget {
         title: const Text("Login"),
         backgroundColor: Colors.deepPurpleAccent,
       ),
+      body: Column(children: [
+        Text("Hi, $email \n Your password is $password"),
+        const Text("If you want to go back the home page, press the button below"),
+        ElevatedButton(onPressed: (){
+
+        }, child: const Text("Back To Home"))
+      ]),
     );
   }
 }
