8b1b34cc86086a43daff75b3a41f5c2b6ba3e6bc
Alireza Naseri <alireza.real98@gmail.com>
2022-09-18 09:47:13 +0430

    add constructor for LoginPage Class

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({required String email, required String password});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: const Icon(Icons.arrow_back),
-      title: const Text("Login"),
-      backgroundColor: Colors.deepPurpleAccent,),
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: const Text("Login"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
     );
   }
 }
