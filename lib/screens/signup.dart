947789b87285d4f6dbc2cc61ac51d99ef80132fe
Alireza Naseri <alireza.real98@gmail.com>
2022-09-18 11:43:47 +0430

    write body for sign up screen text fields

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({Key? key}) : super(key: key);
@@ -13,7 +11,41 @@ class SignupPage extends StatelessWidget {
         title: const Text("Sign Up"),
         backgroundColor: Colors.deepPurpleAccent,
       ),
-
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(children: const [
+          Padding(
+            padding: EdgeInsets.only(bottom: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Name",
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.deepPurpleAccent, width: 2))),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Email",
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.deepPurpleAccent, width: 2))),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Password",
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.deepPurpleAccent, width: 2))),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
