edf4c8e36725a458596ee58e3cf72afb6e2de28a
Alireza Naseri <alireza.real98@gmail.com>
2022-09-18 11:32:20 +0430

    write app bar for sign up screen

---
+++ lib/screens/signup.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: const Text("Sign Up"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+
+    );
+  }
+}
